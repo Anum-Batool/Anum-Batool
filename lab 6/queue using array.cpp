291826650d01fc52088307f0a8354ebed213b02b
Anum-Batool <144526542+Anum-Batool@users.noreply.github.com>
2023-10-22 22:43:00 +0500

    Add files via upload

---
+++ lab 6/queue using array.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class queue{
+	
+public:
+int queue[5], n = 5, front = - 1, rear = - 1;
+
+void Insert() {
+   int val;
+   if (rear == n - 1)
+   cout<<"Queue Overflow"<<endl;
+   else {
+      if (front == - 1)
+      front = 0;
+      cout<<"Insert the element in queue : "<<endl;
+      cin>>val;
+      rear++;
+      queue[rear] = val;
+   }
+}
+void Delete() {
+   if (front == - 1 || front > rear) {
+      cout<<"Queue Underflow ";
+      return ;
+   } else {
+      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
+      front++;;
+   }
+}
+void Display() {
+   if (front == - 1)
+   cout<<"Queue is empty"<<endl;
+   else {
+      cout<<"Queue elements are : ";
+      for (int i = front; i <= rear; i++)
+      cout<<queue[i]<<" ";
+         cout<<endl;
+   }
+}
+
+};
+int main() {
+
+queue q;
+q.Insert();
+q.Delete();
+q.Display();
+return 0;
+
+}
